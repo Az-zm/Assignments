9495fb2db876d1ebd0bad625f0532ecf8bb0c4a9
Az-zm <azmhussain056@gmail.com>
2026-01-11 08:58:25 +0530

    Initial Commit

---
+++ Task2.py
@@ -0,0 +1,5 @@
+# Task 2: Create a Personalized Greeting
+fname = input("Enter your first name : ")
+lname = input("Enter your last name : ")
+Fullname = fname + " " + lname
+print("Hello",Fullname,"! Welcome to the Python program.")
