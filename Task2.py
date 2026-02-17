432ad21c5af849c820bc719c566e17714f904e52
Az-zm <azmhussain056@gmail.com>
2026-02-17 12:49:18 +0530

    Delete Task2.py

--- Task2.py
+++
@@ -1,5 +0,0 @@
-# Task 2: Create a Personalized Greeting
-fname = input("Enter your first name : ")
-lname = input("Enter your last name : ")
-Fullname = fname + " " + lname
-print("Hello",Fullname,"! Welcome to the Python program.")
